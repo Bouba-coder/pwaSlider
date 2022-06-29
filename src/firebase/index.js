ee80c434d167611ab057bfad60d5903999d6d793
Adam SELLIN <adamsellin1914@gmail.com>
2022-06-29 15:33:28 +0200

    add conf firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAg6n1n4PxoGQl4LxQ4C99j2Mi7OxxVRcg",
+    authDomain: "pwa-firebase-e1679.firebaseapp.com",
+    databaseURL: "https://pwa-firebase-e1679-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "pwa-firebase-e1679",
+    storageBucket: "pwa-firebase-e1679.appspot.com",
+    messagingSenderId: "797355236587",
+    appId: "1:797355236587:web:b934ae57f1618c0fbf72ae",
+    measurementId: "G-PFSVY1P5EJ"
+};
+
+export const app = initializeApp(firebaseConfig);
